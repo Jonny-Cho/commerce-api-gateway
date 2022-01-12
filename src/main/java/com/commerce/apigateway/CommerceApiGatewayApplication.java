b8986d0f246ad07f1de569a19fb58492bb9267f2
Jonny-Cho <andante2183@gmail.com>
2022-01-12 22:08:20 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/commerce/apigateway/CommerceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/commerce/apigateway/CommerceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
