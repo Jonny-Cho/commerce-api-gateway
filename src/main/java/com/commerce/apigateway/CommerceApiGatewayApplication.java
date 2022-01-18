84fcbf05e30bc183da423c10ebfceab54c57a1b5
Jonny-Cho <andante2183@gmail.com>
2022-01-18 22:44:18 +0900

    feat: spring cloud config 적용

--- src/main/java/com/commerce/apigateway/CommerceApiGatewayApplication.java
+++ src/main/java/com/commerce/apigateway/CommerceApiGatewayApplication.java
@@ -1,13 +1,21 @@
 package com.commerce.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CommerceApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommerceApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
